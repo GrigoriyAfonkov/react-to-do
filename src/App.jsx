5b5e1106270929edfba32f3192ffe2c87bbeb395
GrigoriyAfonkov <gregarmon@ya.ru>
2026-06-01 01:32:47 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//управляет списком задач, сохраняет его в localStorage, рендерит форму и список
 import { useState, useEffect } from 'react';
 import TodoForm from './components/TodoForm';
 import TodoItem from './components/TodoItem';
